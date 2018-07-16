6a68deb3245883fe543143b91f876a513d9cd31c
Markus Felten <Markus.Felten@gmx.de>
2018-07-17 00:20:49 +0200

    chore(package): add rollup-plugin-istanbul@^2.0.1 chore(package): add rollup-plugin-executable@^1.2.0 chore(rollup): import executable chore(rollup): import resolve chore(rollup): import commonjs chore(rollup): import istanbul chore(rollup): add istanbul to plugins chore(rollup): add resolve to plugins chore(rollup): add commonjs to plugins

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,3 +1,7 @@
+import istanbul from 'rollup-plugin-istanbul';
+
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
@@ -20,5 +24,7 @@ export default {
     'tempy',
     'repository-provider'
   ],
-  plugins: [multiEntry()]
+  plugins: [multiEntry(), istanbul({
+    exclude: ['tests/**/*-test.js']
+  }), resolve(), commonjs()]
 };
