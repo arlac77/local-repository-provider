c0be09374c3c5b641cbb7bd051c3c8e95c37bfc7
Markus Felten <Markus.Felten@gmx.de>
2020-09-23 23:42:28 +0200

    test: update fixture

--- tests/provider-test.mjs
+++ tests/provider-test.mjs
@@ -18,7 +18,7 @@ test("local provider optionsFromEnvironment", t => {
 
 test("local provider", t => {
   const provider = new LocalProvider();
-  t.deepEqual(provider.cloneOptions, ["--depth", "10", "--no-single-branch"]);
+  t.deepEqual(provider.cloneOptions, ["--depth", "8", "--no-single-branch"]);
   t.truthy(provider.workspace.length > 2);
 });
 
