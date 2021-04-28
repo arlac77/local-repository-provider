0077907554917def7748241272acf836d84b78ba
Markus Felten <Markus.Felten@gmx.de>
2021-04-28 15:57:05 +0200

    test: some fixtures disabled

--- tests/repository-test.mjs
+++ tests/repository-test.mjs
@@ -31,17 +31,17 @@ const repoFixtures = {
     condensedName: "sync-test-repository",
     provider: LocalProvider
   },
-  "ssh://git@github.com/arlac77/sync-test-repository.git": {
+ /* "ssh://git@github.com/arlac77/sync-test-repository.git": {
     condensedName: "sync-test-repository",
     provider: LocalProvider
-  }
+  }*/
 
 };
 
 test("locate repository several", async t => {
   const provider = new LocalProvider();
 
-  t.plan(11);
+  t.plan(7);
 
   for (const [url, repoFixture] of Object.entries(repoFixtures)) {
     await assertRepo(t, await provider.repository(url), repoFixture, url);
