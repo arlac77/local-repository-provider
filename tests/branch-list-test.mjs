c064c8b0ee979c89fd86e3246c0c5b72eaf8a672
k0nsti <kkarpman@clickforit.de>
2021-04-29 10:42:27 +0200

    test modified

--- tests/branch-list-test.mjs
+++ tests/branch-list-test.mjs
@@ -8,10 +8,10 @@ test(branchListTest, provider, undefined, 0);
 test(branchListTest, provider, "invalid", 0);
 test(branchListTest, provider, "*", 0);
 
-test(branchListTest, provider, "https://github.com/arlac77/local-repository-provider.git", {
+test.only(branchListTest, provider, "local-repository-provider", {
   "https://github.com/arlac77/local-repository-provider.git" : {
     name: "master",
     fullName: "https://github.com/arlac77/local-repository-provider.git#master",
-    fullCondensedName: "https://github.com/arlac77/local-repository-provider.git"
+    fullCondensedName: "local-repository-provider"
   }
 });
