7973f8c8d29e8c6d236c25dea4694f58da900e23
Markus Felten <Markus.Felten@gmx.de>
2022-01-09 17:37:22 +0100

    test: string getter not removed getString()

--- tests/provider-test.mjs
+++ tests/provider-test.mjs
@@ -85,7 +85,7 @@ test("local get file", async t => {
 
   const file = await branch.entry("README.md");
 
-  t.is((await file.getString()).substring(0, 3), `fil`);
+  t.is((await file.string).substring(0, 3), `fil`);
 });
 
 test.serial("local provider list files", async t => {
@@ -160,8 +160,7 @@ test.serial.skip("local provider commit files", async t => {
     const repository = await provider.repository(REPOSITORY_NAME_GITHUB_GIT);
     const branch = await repository.defaultBranch;
     const file = await branch.entry("README.md");
-    const options = { encoding: "utf8" };
-    const old = await file.getString(options);
+    const old = await file.string;
 
     t.false(!(await file.isEmpty));
     await file.setString(`${old}\n${new Date()}`);
@@ -170,12 +169,12 @@ test.serial.skip("local provider commit files", async t => {
     const file2 = await branch.entry("README.md");
     t.false(!(await file2.isEmpty));
 
-    t.is(await file.getString(options), await file2.getString(options));
+    t.is(await file.string, await file2.string);
 
     await branch.commit("test: ignore", [file]);
 
     const file3 = await branch.entry("README.md");
-    t.is(await file.getString(options), await file3.getString(options));
+    t.is(await file.string, await file3.string);
   } else {
     t.is(1, 1, "skip git@ test without SSH_AUTH_SOCK");
   }
