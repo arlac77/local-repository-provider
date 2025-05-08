d2be1edef07d27f2e52f05f54163fa467b2632c7
Markus Felten <Markus.Felten@gmx.de>
2025-05-08 15:55:44 +0100

    fix: do not create clone into dir explicitly

--- src/local-repository.mjs
+++ src/local-repository.mjs
@@ -105,7 +105,7 @@ export class LocalRepository extends Repository {
       }
     } catch (e) {
       if (e.code === "ENOENT") {
-        await mkdir(this.workspace, { recursive: true });
+        //await mkdir(this.workspace, { recursive: true });
 
         try {
           await this.exec(
